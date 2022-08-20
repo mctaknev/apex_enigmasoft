7085ecf2300ef17d8fb01a0db86d5031869f0a16
mctaknev <100849698+mctaknev@users.noreply.github.com>
2022-08-20 13:21:40 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// var scroll= function () {
+//   document.scrollingElement(function () {
+//     var nav = ".nav";
+//     nav.toggleClass("scrolled", this.scrollTop() > nav.height());
+//   });
+// };
